ee6454bf59ed9c86e97772a59b7e549b32603ea7
Arnout Reitsma <arnout.reitsma@infi.nl>
2023-09-01 14:07:43 +0200

    beter zo

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,35 +1,26 @@
 import { collection, getDocs } from "firebase/firestore";
 import MusicComponent from "./MusicComponent";
-import { Song, songData2 as backUpSongData } from "./Song";
 import { db } from "./firebaseConfig";
 
-export async function fetchData(): Promise<Song[]> {
-  try {
-    const querySnapshot = await getDocs(
-      collection(db, "toonra-song-collection")
-    );
-    const songsData: Song[] = querySnapshot.docs
-      .map(
-        (doc) =>
-          ({
-            title: doc.data().title,
-            url: doc.data().url,
-            album: doc.data().album,
-            genre: doc.data().genre,
-          }) as Song
-      )
-      .filter((d) => d.url != "deleted") as Song[];
-    return songsData;
-  } catch {
-    return backUpSongData;
-  }
+async function fetchSongData() {
+  const querySnapshot = await getDocs(collection(db, "toonra-song-collection"));
+  const songsData = querySnapshot.docs
+    .map((doc) => ({
+      title: doc.data().title,
+      url: doc.data().url,
+      album: doc.data().album,
+      genre: doc.data().genre,
+    }))
+    .filter((d) => d.url != "deleted");
+  return songsData;
 }
 
 async function pageMusic() {
-  const songData: Song[] = await fetchData();
+  const songData = await fetchSongData();
 
   return (
     <div>
+      {" "}
       <MusicComponent songData={songData}></MusicComponent>
     </div>
   );
