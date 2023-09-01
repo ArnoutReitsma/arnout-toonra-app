565a87f2141cee6c212b29617543aee6903fdc02
Arnout Reitsma <arnout.reitsma@infi.nl>
2023-09-01 12:48:29 +0200

    toastjes lekker man

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,16 +1,22 @@
 import { collection, getDocs } from "firebase/firestore";
 import MusicComponent from "./MusicComponent";
-import { Song } from "./Song";
+import { Song, songData2 } from "./Song";
 import { db } from "./firebaseConfig";
 
 export const fetchData = async () => {
   const querySnapshot = await getDocs(collection(db, "toonra-song-collection"));
-  const songsData = querySnapshot.docs.map<Song>((doc) => ({
-    title: doc.data().title,
-    url: doc.data().url,
-    album: doc.data().album,
-    genre: doc.data().genre,
-  })).filter(d => d.url != 'deleted');
+  if (!querySnapshot) {
+    return songData2;
+  }
+  const songsData = querySnapshot.docs
+    .map<Song>((doc) => ({
+      title: doc.data().title,
+      url: doc.data().url,
+      album: doc.data().album,
+      genre: doc.data().genre,
+    }))
+    .filter((d) => d.url != "deleted");
+
   return songsData;
 };
 
